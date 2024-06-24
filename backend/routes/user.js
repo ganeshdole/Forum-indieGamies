0aae1d47d3f329f923f2ad1fd0de0eb110a1f7dd
Ganesh Dole <dole77770@gmail.com>
2024-06-24 11:51:53 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser , updateUserById, deleteUserById, signinUser} = require('../controllers/userController');
+const { registerUser, updateUserById, deleteUserById, signinUser, getUserById } = require('../controllers/userController');
 
 router.post('/register', registerUser);
-router.post("/signin", signinUser );
-router.delete("/deleteuser", deleteUserById)
-router.patch("/updateuser", updateUserById)
+router.post('/signin', signinUser);
+router.get('/me', getUserById); 
+router.patch('/update', updateUserById);
+router.delete('/delete', deleteUserById);
 
 module.exports = router;
