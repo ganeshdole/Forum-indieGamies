9443b20f1e5d0cda757c68ba2c67afae67c1235d
Ganesh Dole <dole77770@gmail.com>
2024-06-24 18:06:26 +0530

    get Threads By Id

--- frontend/src/services/threads.js
+++ frontend/src/services/threads.js
@@ -1,17 +1,24 @@
 import axios  from "axios";
 import { createError, createUrl } from "./utils";
 
-
 export async function getAllThreads(){
     try{
-        const result = await axios(createUrl('threads'));
+        const result = await axios(createUrl('threads'),{});
         return result.data;
     }catch(error){
         console.log(error);
         return createError(error);
     }
 }
-
+export async function getThreadById(threadId){
+    try{
+        const result = await axios(createUrl(`threads/thread/${threadId}`))
+        return result.data
+    }catch(error){
+        console.log(error)
+        return createError(error);
+    }
+}
 export async function getThreadsBYCategory(categoryId){
     try{
         const result = await axios(createUrl(`threads/${categoryId}`))
