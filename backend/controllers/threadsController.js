d13f7181ab927bc3749cdbe0a830bf0a995f04d6
Ganesh Dole <dole77770@gmail.com>
2024-06-24 11:51:19 +0530

    [Proper naming] change getThreads to getLatestThread

--- backend/controllers/threadsController.js
+++ backend/controllers/threadsController.js
@@ -2,17 +2,18 @@ const threadsModel = require('../db/models/threadsModel')
 const { createSuccess, createError } = require('../utils/utils')
 
 
-const getThreads = async (req, res) => {
+const getLatestThread = async (req, res) => {
     try {
         const threads = await threadsModel.find();
-        console.log('Threads:', threads);
+        if(threads.length === 0){
+            res.status(500).json(createError('Error getting threads', error.message));
+        }
         res.status(200).json(createSuccess(threads));
     } catch (error) {
         console.error('Error getting threads:', error.message);
         res.status(500).json(createError('Error getting threads', error.message));
     }
 }
-
 module.exports = {
-    getThreads
+    getLatestThread
 }
