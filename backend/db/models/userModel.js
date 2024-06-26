24e3f4bd67612759453aeef2ad8fa04d1f99d231
Ganesh Dole <dole77770@gmail.com>
2024-06-26 16:21:15 +0530

    name changes to username

--- backend/db/models/userModel.js
+++ backend/db/models/userModel.js
@@ -1,39 +1,34 @@
-const mongoose = require('mongoose')
-const userSchema = new mongoose.Schema({
-    name:{
-        firstName: {
-            type:String,
-            required :true,
-            trim: true,
+const mongoose = require('mongoose');
 
-        },
-        lastName : {
-            type : String,
-            required : true,
-            trim : true
-        }
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
     },
-    email : {
-        type : String,
-        required : true,
-        unique : true, 
+    email: {
+        type: String,
+        required: true,
+        unique: true,
         lowercase: true,
-        trim : true,
-        match: [/.+@.+\..+/,'Invalid Email Format']
+        trim: true,
+        match: [/.+@.+\..+/, 'Invalid Email Format']
     },
-    role:{
+    role: {
         type: Number,
-        default : 0,
-        enum : [0, 1 , 2]
+        default: 0,
+        // enum: [0, 1, 2], // 0: User, 1: Moderator, 2: Admin
     },
     password: {
         type: String,
-        required : true, 
+        required: true,
         minlength: 8
     }
-})
+}, {
+    timestamps: true 
+});
 
-const userModel = new mongoose.model('users', userSchema)
+const userModel = mongoose.model('User', userSchema);
 
 module.exports = userModel;
-
