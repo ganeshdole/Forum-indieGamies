fcc718bb0c7e709c9f142a6f01913490b3c2b0c3
Ganesh Dole <dole77770@gmail.com>
2024-06-28 14:00:58 +0530

    Update Threads.jsx

--- frontend/src/pages/Thread/Threads.jsx
+++ frontend/src/pages/Thread/Threads.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getThreadById } from "../../../services/threads";
-import { getCategoryById } from "../../../services/categoris";
-
+import { getThreadById } from "../../services/threads";
+import { getCategoryById } from "../../services/categoris";
 import { getRepliesByThreadId } from "../../services/replies";
 
 const Threads = () => {
